efa053842a0d753059e69133a89c0ede773b3fa9
Arik Rinberg <ArikRinberg@gmail.com>
2017-02-23 20:56:33 +0200

    There is a bug

--- myfile.py
+++ myfile.py
@@ -1,4 +1,4 @@
-
+import sys
 
 class Video:
     def __init__(self, id, size):
@@ -21,18 +21,24 @@ class Endpoint:
 
     def __init__(self, caches, lat):
         caches = caches
-        requests = {}
+        requests = dict()
         dc_lat = lat
 
     def add_request(self, id, num_reqs):
         self.requests[id] = num_reqs
 
 
-def calc_score(caches, endpoint):
+def calc_endpoint_score(caches, endpoint):
+    score = 0
+    # calculate score from each request
     for req in endpoint.requests.keys():
         min_lat = endpoint.dc_lat
         for c in caches:
-            if req in caches.vids and c in :
+            # requested video is in cache and cache is connected to endpoint
+            if req in caches.vids and c in endpoint.caches.keys():
+                if endpoint.caches[c] < min_lat:
+                    min_lat = endpoint.caches[c]
+        score += min_lat * endpoint.requests[req]
 
 
 videoSizes = dict()
@@ -47,7 +53,7 @@ cacheSize = 0
 def init_data(fname):
     global nVideos, nEndpoints, nRequests, nCaches, cacheSize, videoSizes, endpointsDict
     try:
-        with open(fname) as fhanlder:
+        with open(fname, 'r') as fhanlder:
             initline = fhanlder.readline().split()
             nVideos = int(initline[0])
             nEndpoints = int(initline[1])
@@ -66,15 +72,27 @@ def init_data(fname):
                 endpointsDict[i] = Endpoint(connection_latencies, latency)
             for i in range(0, nRequests):
                 request_line = fhanlder.readline().split()
+                print " ".join(request_line)
                 endpointsDict[int(request_line[1])].add_request(int(request_line[0]), int(request_line[2]))
     except IOError:
         print "Couldn't find ", fname
         raise
 
 
+def selfcheck():
+    print "nVideos", nVideos
+    print "nEndpoints", nEndpoints
+    print "nRequests", nRequests
+    print "nCaches", nCaches
+    print "cacheSize", cacheSize
+    for k in videoSizes.keys():
+        print videoSizes[k] + " ",
+    print
+
 def main():
     fname = sys.argv[1]
-
+    init_data(fname)
+    selfcheck()
 
 if __name__ == '__main__':
     main()
