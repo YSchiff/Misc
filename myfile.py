c0e4cab3a1b19c1f4f15c219730bc0e54476bfee
Arik Rinberg <ArikRinberg@gmail.com>
2017-02-23 20:34:49 +0200

    Added init

--- myfile.py
+++ myfile.py
@@ -1,9 +1,45 @@
 import sys
 
 
+videoSizes = dict()
+endpointsDict = dict()
+nVideos = 0
+nEndpoints = 0
+nRequests = 0
+nCaches = 0
+cacheSize = 0
+
+
+def init_data(fname):
+    global nVideos, nEndpoints, nRequests, nCaches, cacheSize, videoSizes, endpointsDict
+    try:
+        with open(fname) as fhanlder:
+            initline = fhanlder.readline().split()
+            nVideos = int(initline[0])
+            nEndpoints = int(initline[1])
+            nRequests = int(initline[2])
+            nCaches = int(initline[3])
+            cacheSize = int(initline[4])
+            for i, size in enumerate(fhanlder.readline().split()):
+                videoSizes[i] = Video(i, int(size))
+            for i in range(0, nEndpoints):
+                endpoint_line = fhanlder.readline().split()
+                latency = int(endpoint_line[0])
+                connection_latencies = dict()
+                for j in range(0, int(endpoint_line[1])):
+                    line = fhanlder.readline().split()
+                    connection_latencies[int(line[0])] = int(line[1])
+                endpointsDict[i] = Endpoint(connection_latencies, latency)
+            for i in range(0, nRequests):
+                request_line = fhanlder.readline().split()
+                endpointsDict[int(request_line[1])].add_request(int(request_line[0]), int(request_line[2]))
+    except IOError:
+        print "Couldn't find ", fname
+        raise
+
 
 def main():
-    
+    fname = sys.argv[1]
 
 
 if __name__ == '__main__':
